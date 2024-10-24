f2372e5e31ebf540e6b38128be24ea517d5d4855
bembengm1906 <146632077+bembengm1906@users.noreply.github.com>
2024-10-24 20:11:15 +0200

    Update convaccelerator.hpp

--- convaccelerator.hpp
+++ convaccelerator.hpp
@@ -8,12 +8,17 @@
 #include <hls_vector.h>
 
 
-
 #define INPUT_SIZE      1300U
 #define KERNEL_SIZE     3U
 #define OUT_SIZE   		(INPUT_SIZE - KERNEL_SIZE + 1)
 #define TILE_SIZE		120
 #define VECTOR_SIZE		4
+#define TILE_H			120
+#define TILE_W			120
+
+// Define a custom data width (e.g., 128 bits)
+#define CUSTOM_DATAWIDTH 128
+typedef ap_uint<CUSTOM_DATAWIDTH> uintX_t;
 
 void CPUConvolution(int *image, int *kernel, int *output);
 void MatrixGen(int* image, uint32_t rows, uint32_t cols);
@@ -22,27 +27,14 @@ void Compare(int* ACC_OUT, int* CPU_OUT, uint32_t rows, uint32_t cols, int* errC
 
 extern"C"{
 void convolve_all(
-    hls::stream<int> &input0_stream,
-    hls::stream<int> &input1_stream,
-    hls::stream<int> &input2_stream,
-    hls::stream<int> &input3_stream,
-    hls::stream<int> &input4_stream,
-    hls::stream<int> &input5_stream,
-    hls::stream<int> &input6_stream,
-    hls::stream<int> &input7_stream,
-    hls::stream<int> &input8_stream,
-    hls::stream<int> &input9_stream,
-    hls::stream<int> &input10_stream,
-    hls::stream<int> &output0_stream,
-    hls::stream<int> &output1_stream,
-    hls::stream<int> &output2_stream,
-    hls::stream<int> &output3_stream,
-    hls::stream<int> &output4_stream,
-    hls::stream<int> &output5_stream,
-    hls::stream<int> &output6_stream,
-    hls::stream<int> &output7_stream,
-    hls::stream<int> &output8_stream,
-    hls::stream<int> &output9_stream,
-    hls::stream<int> &output10_stream);}
+    int *subimage0, int *subimage1, int *subimage2,
+    int *subimage3, int *subimage4	, int *subimage5
+    ,int *output0, int *output1, int *output2, int *output3,
+    int *output4, int *output5);
+
+
+void FixedConvolution(
+        int *image,
+        int *output);}
 
 #endif
